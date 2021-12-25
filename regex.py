65e74fe7ded4a8e646c8b44f43ba04e28b6b59c9
Kaspars <kasparslie@gmail.com>
2021-12-25 22:26:19 +0100

    added work instructions as comment

--- regex.py
+++ regex.py
@@ -1,3 +1,7 @@
+#The basic outline of this problem is to read the file, 
+# look for integers using the re.findall(), looking for a regular expression of '[0-9]+'
+# and then converting the extracted strings to integers and summing up the integers.
+
 import re
 file = input("Enter file:")
 if len(file) < 1:
